584c4697bfa7775b5b7a68eb0497406229091d4b
Dmytro Naumchak <51541064+DrMarek@users.noreply.github.com>
2019-07-07 14:58:47 +0300

    Update task1.js

--- FL11_HW6/homework/js/task1.js
+++ FL11_HW6/homework/js/task1.js
@@ -1,21 +1,20 @@
-var a1=0, a2=0, b1=0, b2=0, c1=0, c2=0, tmpx=0, tmpy=0;
-var t=true, f=false;
-
 alert("ATENTION! \n If you use float numbers plese write dot instead of comma");
 
-a1=Number(prompt("input a(x)"));
-a2=Number(prompt("input a(y)"));
-b1=Number(prompt("input b(x)"));
-b2=Number(prompt("input b(y)"));
-c1=Number(prompt("input c(x)"));
-c2=Number(prompt("input c(y)"));
+let a1=Number(prompt("input a(x)"));
+let a2=Number(prompt("input a(y)"));
+let b1=Number(prompt("input b(x)"));
+let b2=Number(prompt("input b(y)"));
+let c1=Number(prompt("input c(x)"));
+let c2=Number(prompt("input c(y)"));
+let divide=2;
+let t= true;
+let f= false;
+let tmpx=(a1+b1)/divide;
+let tmpy=(a2+b2)/divide;
 
-tmpx=(a1+b1)/2;
-tmpy=(a2+b2)/2;
 
 if(tmpx===c1 && tmpy===c2){
     console.log(t);
-}
-else{
+}else{
     console.log(f);
 }
