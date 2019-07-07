4d7264659f2f42a2a24afca03fd4fa06aca07011
Dmytro Naumchak <51541064+DrMarek@users.noreply.github.com>
2019-07-07 14:27:08 +0300

    Update task2.js

--- FL11_HW6/homework/js/task2.js
+++ FL11_HW6/homework/js/task2.js
@@ -7,10 +7,10 @@ c=Number(prompt("Input c"));
 if(a<=0 || b<=0 || c<=0){
     console.log("Triangle doesn’t exist");
 }
-else if(a==b && b==c && c==a){
+else if(a===b && b===c && c===a){
     console.log("Eequivalent triangle");
 }
-else if(a==b || b==c || c==a){
+else if(a===b || b===c || c===a){
     console.log("Isosceles triangle");
 }
 else{
